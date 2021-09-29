483a7f32c07f489a31b397ac1b4894d9ff6d8d3d
dhef415 <dhef415@gmail.com>
2021-09-28 21:35:57 -0700

    server up

--- api/server.js
+++ api/server.js
@@ -1,11 +1,18 @@
 const express = require('express');
-
+const usersRouter = require('./users/users-router')
 const server = express();
 
-// remember express by default cannot parse JSON in request bodies
+server.use(express.json())
+
+server.use('api/users', usersRouter)
 
 // global middlewares and the user's router need to be connected here
-
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  })
+})
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
