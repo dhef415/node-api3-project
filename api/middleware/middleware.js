a7badcb589c40e5fd6172749640608e4013537e7
dhef415 <dhef415@gmail.com>
2021-09-28 21:50:46 -0700

    logger

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,29 @@
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  const timeStamp = new Date().toLocaleString()
+  const method = req.method
+  const url = req.originalURL
+  console.log(`[${timeStamp}] ${method} to ${url}`)
+  next()
 }
 
 function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+  console.log('validateUserId')
+  next()
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  console.log('validateUser')
+  next()
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  console.log('validatePost')
+  next()
 }
 
-// do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost,
+}
--- api/server.js
+++ api/server.js
@@ -1,20 +1,23 @@
-const express = require('express');
-const usersRouter = require('./users/users-router')
-const server = express();
+const express = require('express')
+const server = express()
+// const usersRouter = require('./users/users-router')
+const { logger } = require('./middleware/middleware')
 
 server.use(express.json())
 
-server.use('api/users', usersRouter)
+server.use(logger)
+// server.use('api/users', usersRouter)
 
 // global middlewares and the user's router need to be connected here
-server.use((err, req, res, next) => {
-  res.status(err.status || 500).json({
-    message: err.message,
-    stack: err.stack,
-  })
-})
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
-});
+// server.use((err, req, res, next) => {
+//   res.status(err.status || 500).json({
+//     message: err.message,
+//     stack: err.stack,
+//   })
+// })
 
-module.exports = server;
+server.get('/', (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`)
+})
+
+module.exports = server
